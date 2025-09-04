e04ed46263fcb6ab6314dce5880194db09e742db
Sami Mäkinen <sami.makinen@nls.fi>
2025-09-04 10:24:48 +0300

    Improve naming and comment

--- service-coordtransform/src/main/java/fi/nls/paikkatietoikkuna/coordtransform/KomuBundleHandler.java
+++ service-coordtransform/src/main/java/fi/nls/paikkatietoikkuna/coordtransform/KomuBundleHandler.java
@@ -9,7 +9,7 @@ import fi.nls.oskari.view.modifier.ModifierParams;
 import org.json.JSONObject;
 
 /**
- * Injects possible url for frontend in komu config
+ * Injects an optional alternative url for frontend to use through bundle config
 
  {
  "url": "/action/KomuProj"
@@ -20,16 +20,16 @@ public class KomuBundleHandler extends BundleHandler {
 
     public boolean modifyBundle(final ModifierParams params) throws ModifierException {
         final JSONObject config = getBundleConfig(params.getConfig());
-        JSONHelper.putValue(config, "url", isFrontendUrl());
+        JSONHelper.putValue(config, "url", getAltFrontendUrl());
         return false;
     }
 
     /**
-     * If users are managed in external source any changes to them are usually overwritten when they login.
-     * So we can disable the fields that are and updates that can happen to users to make the admin UI more user-friendly.
+     * Allows configuring an alternative frontend URL for the frontend to use when calling the server.
+     * Either a path to a new route on this server (/action/KomuProj) or a whole URL to another service (komu backend)
      * @return
      */
-    public static String isFrontendUrl() {
+    public static String getAltFrontendUrl() {
         return PropertyUtil.getOptional("pti.komu.frontendUrl");
     }
 }
